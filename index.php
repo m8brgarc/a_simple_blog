3e61fb4cae02d4d3f800cb900977bc7c7d961e9f
Brandon G.A <brandonkeoni@gmail.com>
2014-08-10 01:29:30 -0700

    Added basic html, starting css/js files, link tag to Google CDN

--- index.php
+++ index.php
@@ -3,4 +3,20 @@ require_once 'init.php';
 
 ?>
 
-Hello! World!
+<!DOCTYPE html>
+<html>
+<head>
+    <title>A Simple Blog</title>
+    <link href="css/main.css" rel="stylesheet" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <header>
+        <h2>A Simple Blog</h2>
+    </header>
+    <div id="wrapper">
+
+    </div>
+</body>
+</html>
